f9cc662e15f4ae4362234e8b1582642e44542a38
SyamGaraga <syamgaragaabc@gmail.com>
2025-02-18 22:37:05 +0530

    msg1

--- nodejs/main.js
+++ nodejs/main.js
@@ -1,7 +1,7 @@
 const http=require("http");
 const server=http.createServer((req,res)=>{
     res.write("E chadavatalu passavatalu mana valla kadhura physicalgani mentalgani strainuu");
-    res.end("response provided")
+    res.end()
 });
 server.listen("3110",()=>{console.log("server running")})
 
