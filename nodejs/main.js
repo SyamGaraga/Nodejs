ff99dfed1342501cb5ee71bfb48bc91a5fd49a94
SyamGaraga <syamgaragaabc@gmail.com>
2025-02-19 08:33:13 +0530

    msg

--- nodejs/main.js
+++ nodejs/main.js
@@ -1,7 +1,13 @@
 const http=require("http");
+// const server=http.createServer((req,res)=>{
+//     res.write("E chadavatalu passavatalu mana valla kadhura physicalgani mentalgani strainuu");
+//     res.end()
+// });
+let obj={name:"syam",city:"Amp"};
 const server=http.createServer((req,res)=>{
-    res.write("E chadavatalu passavatalu mana valla kadhura physicalgani mentalgani strainuu");
-    res.end()
+    res.writeHead(201,"hello Syam",{"content-type":"application/json"});
+    res.write(JSON.stringify(obj));
+    res.end();
 });
 server.listen("3110",()=>{console.log("server running")})
 
